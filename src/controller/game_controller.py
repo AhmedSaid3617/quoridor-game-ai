29d419364c14c5d52891099d1ec413aa3074deef
Shams M. Monem <shamsmmonem@gmail.com>
2025-12-13 12:38:51 +0200

    StateController seperated from GameController

--- src/controller/game_controller.py
+++ src/controller/game_controller.py
@@ -1,3 +1,4 @@
+from src.controller.state_controller import StateController
 from src.rules.game_rules import GameRules
 from src.state.game_state import GameState
 
@@ -6,6 +7,7 @@ class GameController:
     def __init__(self, game_state: GameState, starting_player: GameState.Player):
         
         self.game_state = game_state
+        self.state_controller = StateController(self.game_state)
         self.current_player = starting_player
         self.rules = GameRules()
 
@@ -21,17 +23,12 @@ class GameController:
 
         if isinstance(move, GameRules.PawnMove):
             if self.rules.can_apply_pawn_move(move, self.game_state.active_player):     # If this player can move to this position.
-                if move.system == GameRules.PawnMove.SystemType.ABSOLUTE:
-                    self.game_state.place_player(self.game_state.active_player, move.position)  # Then move him.
-                else:
-                    position  = self.game_state.player_one if self.game_state.active_player == GameState.Player.PLAYER_ONE else self.game_state.player_two
-                    self.game_state.place_player(self.game_state.active_player, position + GameRules.movement_to_delta(move.movement))  # Then move him.
-                
+                self.state_controller.apply_pawn_move(self.game_state.active_player, move)
                 return True
             
         elif isinstance(move, GameRules.WallMove):
             if self.rules.can_apply_wall_move(self.game_state.active_player, move): # If this player can play this wall move.
-                self.game_state.place_wall(move.wall, move.position) # Then place a wall at this postion.
+                self.state_controller.apply_wall_move(self.game_state.active_player, move)
                 return True
                 
         else:
---
+++ src/controller/state_controller.py
@@ -0,0 +1,22 @@
+from src.rules.game_rules import GameRules
+from src.state.game_state import GameState
+
+
+class StateController:
+    def __init__(self, game_state: GameState):
+        self.game_state = game_state
+
+    def apply_pawn_move(self, player: GameState.Player, move: GameRules.PawnMove):
+        if move.system == GameRules.PawnMove.SystemType.ABSOLUTE:
+            self.game_state.place_player(player, move.position)
+        else:
+            position  = self.game_state.player_one if self.game_state.active_player == GameState.Player.PLAYER_ONE else self.game_state.player_two
+            self.game_state.place_player(player, position + GameRules.movement_to_delta(move.movement))
+
+    def apply_wall_move(self, player: GameState.Player, move: GameRules.WallMove):
+        if player == GameState.Player.PLAYER_ONE:
+            self.game_state.player_one_remaining_walls -= 1
+        else:
+            self.game_state.player_two_remaining_walls -= 1
+
+        self.game_state.place_wall(move.wall, move.position)
